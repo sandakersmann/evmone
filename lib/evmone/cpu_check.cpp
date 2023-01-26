70069863cf785115828e46b456f225739f045f2b
Paweł Bylica <pawel@ethereum.org>
2023-01-26 23:11:00 +0100

    support GCC 11

--- lib/evmone/cpu_check.cpp
+++ lib/evmone/cpu_check.cpp
@@ -2,13 +2,14 @@
 #define STRINGIFY_HELPER(X) #X
 #define STRINGIFY(X) STRINGIFY_HELPER(X)
 
-#ifdef __clang__
-// Clang 16 does not support architecture levels in __builtin_cpu_supports(). Use approximations.
+#if defined(__GNUC__) && __GNUC__ >= 12
+#define CPU_FEATURE "x86-64-v" STRINGIFY(EVMONE_X86_64_ARCH_LEVEL)
+#else
+// Clang 16 and GCC 11 does not support architecture levels in __builtin_cpu_supports().
+// Use approximations.
 #if EVMONE_X86_64_ARCH_LEVEL == 2
 #define CPU_FEATURE "sse4.2"
 #endif
-#elif __GNUC__
-#define CPU_FEATURE "x86-64-v" STRINGIFY(EVMONE_X86_64_ARCH_LEVEL)
 #endif
 
 #ifndef CPU_FEATURE
