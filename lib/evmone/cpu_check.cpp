dea8ba7449a6d2bcab67f7ec5a3bc7617ace4a8a
Paweł Bylica <pawel@ethereum.org>
2023-01-26 22:55:49 +0100

    typo

--- lib/evmone/cpu_check.cpp
+++ lib/evmone/cpu_check.cpp
@@ -3,7 +3,7 @@
 #define STRINGIFY(X) STRINGIFY_HELPER(X)
 
 #ifdef __clang__
-// Clang 16 does not support achitecture levels in __builtin_cpu_supports(). Use approximations.
+// Clang 16 does not support architecture levels in __builtin_cpu_supports(). Use approximations.
 #if EVMONE_X86_64_ARCH_LEVEL == 2
 #define CPU_FEATURE "sse4.2"
 #endif
