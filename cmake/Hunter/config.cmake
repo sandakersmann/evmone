f0c99c2e48b6f2707e48ebd14086ba7f4d21059c
Alex Beregszaszi <alex@rtfs.hu>
2023-02-02 00:12:43 +0100

    f

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,9 +4,9 @@
 
 include(hunter_cmake_args)
 
-hunter_cmake_args(
+hunter_config(
     ethash
-    CMAKE_ARGS -DETHASH_BUILD_ETHASH=OFF -DETHASH_BUILD_TESTS=OFF
+    CMAKE_ARGS ETHASH_BUILD_ETHASH=OFF ETHASH_BUILD_TESTS=OFF
 )
 
 hunter_config(
